fb7ea31ff524c28a35db52c66d379c7c77769b60
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-03-07 22:58:21 -0500

    updated tag-routes.js and product-routes.js

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -135,6 +135,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(dbProductData => {
+    if (!dbProductData) {
+      res.status(404).json({ message: 'Category not deleted due to invalid ID.' });
+      return;
+    }
+    res.json(dbProductData);
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
